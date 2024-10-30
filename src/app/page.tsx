5a17b37d8499f7a7ca468bddf0adb42436f5f014
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-30 06:31:30 +0530

    primsa + login user show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
 
-import { useEffect } from "react";
+import { signOut,signIn } from "@/auth"
+import { auth } from "../auth"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
+  if (!session) {
+    return (
+      <div>
+        <form
+          action={async () => {
+            "use server"
+            await signIn()
+          }}
+        >
+          <button type="submit">Sign in</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <>
       <div>
+        <div>
+          <img src={(session as any).user.image} alt="User Avatar" />
+        </div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+        <form
+          action={async () => {
+            "use server"
+            await signOut()
+          }}
+        >
+          <button type="submit">Sign Out</button>
+        </form>
       </div>
     </>
   );
